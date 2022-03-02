9f97682a6661a7efc0fd3b816c944e13855ed772
knitterJ <knitter.m.jacob@gmail.com>
2022-03-02 22:20:34 +0100

    coding challenge daily D39

---
+++ Warmup-1/Or35.java
@@ -0,0 +1,16 @@
+//Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+public class Or35 {
+
+public static boolean or35(int n) {
+  if((n%5==0) || (n%3==0)){
+    return true;
+  } return false;
+}
+
+public static void main(String[] args){
+  System.out.println(or35(8));
+  System.out.println(or35(10));
+  System.out.println(or35(35));
+
+  }
+}
