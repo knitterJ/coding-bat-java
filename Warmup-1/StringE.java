41e64ec33be69199db9568d68fefcfb61ae836be
knitterJ <knitter.m.jacob@gmail.com>
2022-03-29 03:49:24 +0200

    coding challenge daily 59

---
+++ Warmup-1/StringE.java
@@ -0,0 +1,24 @@
+// Return true if the given string contains between 1 and 3 'e' chars.
+//
+// stringE("Hello") → true
+// stringE("Heelle") → true
+// stringE("Heelele") → false
+
+public class StringE {
+
+    public static boolean stringE(String str) {
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') {
+                counter++;
+            }
+        }
+        return (counter >= 1 && counter <= 3);
+    }
+
+    public static void main(String args[]) {
+        System.out.println(stringE("Hello"));
+        System.out.println(stringE("Heeello"));
+        System.out.println(stringE("Heeeello"));
+    }
+}
