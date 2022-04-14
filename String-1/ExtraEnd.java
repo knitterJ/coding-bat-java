1a39c8b4eb61c7dd1f8cb55801f239f661ec167c
knitterJ <knitter.m.jacob@gmail.com>
2022-04-14 04:05:44 +0200

    coding challenge daily D70

---
+++ String-1/ExtraEnd.java
@@ -0,0 +1,19 @@
+// Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+//
+// extraEnd("Hello") → "lololo"
+// extraEnd("ab") → "ababab"
+// extraEnd("Hi") → "HiHiHi"
+
+public class ExtraEnd{
+
+  public static String extraEnd(String str) {
+    String lastTwo = str.substring(str.length()-2);
+    return lastTwo + lastTwo + lastTwo;
+  }
+
+
+
+public static void main(String[] args){
+  System.out.println(extraEnd("Hi"));
+  }
+}
