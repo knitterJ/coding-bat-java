58d7286cccfde24aa11d8028de3f57e5ddceb2d2
knitterJ <knitter.m.jacob@gmail.com>
2022-05-29 01:04:54 +0200

    coding challenge daily D110

---
+++ String-1/TheEnd.java
@@ -0,0 +1,18 @@
+// Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+//
+// theEnd("Hello", true) → "H"
+// theEnd("Hello", false) → "o"
+// theEnd("oh", true) → "o"
+
+public class TheEnd{
+public static String theEnd(String str, boolean front) {
+  if(front == false){
+    return str.substring(str.length()-1, str.length());
+  } else return str.substring(0,1);
+}
+
+public static void main(String[] args){
+  System.out.println(theEnd("Hello", true));
+  System.out.println(theEnd("Hello", false));
+  }
+}
