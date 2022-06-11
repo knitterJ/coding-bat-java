ec6c28b051d2d925330087f209f5d7fa45805d0d
knitterJ <knitter.m.jacob@gmail.com>
2022-06-11 02:57:34 +0200

    coding challenge daily D120

---
+++ String-1/MiddleThree.java
@@ -0,0 +1,20 @@
+// Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+//
+// middleThree("Candy") → "and"
+// middleThree("and") → "and"
+// middleThree("solving") → "lvi"
+
+public class MiddleThree {
+
+  public static String middleThree(String str) {
+    return str.substring(str.length()/2-1,str.length()/2+2);
+  }
+
+
+
+public static void main(String[] args){
+  System.out.println(middleThree("Candy"));
+  System.out.println(middleThree("and"));
+  System.out.println(middleThree("solving"));
+  }
+}
