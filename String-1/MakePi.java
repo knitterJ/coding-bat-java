f991e31b3a098f7bd72e76e2bfc5017855df2ed5
knitterJ <knitter.m.jacob@gmail.com>
2022-06-30 21:00:58 +0200

    coding challenge daily D134

---
+++ String-1/MakePi.java
@@ -0,0 +1,14 @@
+// Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+//
+// makePi() → [3, 1, 4]
+
+public class MakePi {
+
+  public static int[] makePi() {
+    return new int[] {3,1,4};
+  }
+
+public static void main(String[] args){
+     makePi();
+  }
+}
