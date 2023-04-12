f214dade89d5c37233abe74984a4df5aef420c0a
J <knitter.m.jacob@gmail.com>
2023-04-12 02:02:57 +0200

    coding challenge daily D17

--- Map-2/Word0.java
+++ Map-2/Word0.java
@@ -3,3 +3,20 @@
 // word0(["a", "b", "a", "b"]) → {"a": 0, "b": 0}
 // word0(["a", "b", "a", "c", "b"]) → {"a": 0, "b": 0, "c": 0}
 // word0(["c", "b", "a"]) → {"a": 0, "b": 0, "c": 0}
+
+import java.util.Map;
+import java.util.*;
+public class Word0 {
+
+  public static Map < String, Integer > word0(String[] strings) {
+    Map < String, Integer > map = new HashMap < String, Integer > ();
+    for (String s: strings) {
+      map.put(s, 0);
+    }
+    return map;
+  }
+  public static void main(String[] args) {
+    String[] testArray = {"a","b","a","b"};
+    System.out.println(word0(testArray));
+  }
+}
