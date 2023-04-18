f5937ed05a7a59f6fc6b967a1fe55ba80bc3e7ea
knitterJ <knitter.m.jacob@gmail.com>
2023-04-18 23:25:03 +0100

    coding challenge daily

--- Map-2/WordLen.java
+++ Map-2/WordLen.java
@@ -3,11 +3,22 @@
 // wordLen(["a", "bb", "a", "bb"]) → {"bb": 2, "a": 1}
 // wordLen(["this", "and", "that", "and"]) → {"that": 4, "and": 3, "this": 4}
 // wordLen(["code", "code", "code", "bug"]) → {"code": 4, "bug": 3}
+import java.util.Map;
+import java.util.*;
 
 public class WordLen {
 
-public Map<String, Integer> wordLen(String[] strings) {
+public static Map<String, Integer> wordLen(String[] strings) {
+  Map<String, Integer> mapCount = new HashMap<>();
+  for(String s:strings){
+    mapCount.put(s, s.length());
+  }
+  return mapCount;
+}
 
+public static void main(String[] args) {
+  String[] testArray = {"aaaa","b","aa","bbb"};
+  System.out.println(wordLen(testArray));
 }
 
 }
